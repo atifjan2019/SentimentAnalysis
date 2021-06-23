84ac92668f3d95f5b783e810d9560ae775037645
atifjan2019 <atifjan2019@gmail.com>
2021-06-23 11:45:35 +0500

    Tokenization of a sentence

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import string
 text = open('read.txt',encoding='utf-8').read()
 lower_case = text.lower()
 cleaned_text = lower_case.translate(str.maketrans('','',string.punctuation))
+#print(cleaned_text)
 tokemized_words = cleaned_text.split()
 print(tokemized_words)
