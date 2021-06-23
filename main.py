a73bc223393f672baf42babb60c42d06a5d3868d
atifjan2019 <atifjan2019@gmail.com>
2021-06-23 12:02:37 +0500

    stop words

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ lower_case = text.lower()
 # Removing punctuations
 cleaned_text = lower_case.translate(str.maketrans('','',string.punctuation))
 # splitting text into words
-tokemized_words = cleaned_text.split()
-
+tokenized_words = cleaned_text.split()
+print(tokenized_words)
 stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
               "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
               "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
@@ -17,3 +17,10 @@ stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you"
               "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
               "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
               "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+final_words = []
+for word in tokenized_words:
+    if word not in stop_words:
+        final_words.append(word)
+
+print(final_words)
